27b076621f1045e7c0c0bc4830ca41260ab8ac05
Marquise Campbell <marquisetyrrell@gmail.com>
2023-02-14 14:22:19 -0500

    Loop exercise that took me forever to complete

---
+++ main.js
@@ -0,0 +1,37 @@
+// var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+// var z = 0
+// while (z < cohortOneStudents.length){console.log(cohortOneStudents[z]); z++}
+
+// var hobby = ["Games", "Movies", "Animals", "Family", "Friends" ]
+// var x = 0
+// for (x < hobby.length)
+// {
+// {    if (hobby === "Games")
+//     {console.log(`My absolute Favorite interest is: ${hobby["Games"]}`)}
+// };
+// else
+// {
+// (console.log(`One of my favorite interests is : ${hobby[x]}`))
+//  x++}}
+
+// let interests = ["one", "two", "three", "four", "five"];
+
+// for(var i = 0; i < hobby.length; i++){
+  
+//   if (interests[i] === "one"){
+//     console.log("My absolute favorite interest is: " + interests[i]);
+//   }
+//   else{  
+//     console.log("One of my interests is: " + interests[i]);
+//   }
+
+// }
+
+var hobby = ["Games", "Movies", "Animals", "Family", "Friends"]
+for (var i = 0; i <hobby.length; i++){
+    if (hobby[i] === "Games")
+    {console.log(`My absolute favorite interest is : ${hobby[i]}`)}
+    else{
+        console.log(` One of my favorite interests is: ${hobby[i]}`)
+    }
+}
